e491b388b2b076a7e6ffafd85710f78f06d7f4ca
Richard Hopkins <hopkira@googlemail.com>
2023-12-17 18:40:53 +0000

    Changes

--- ros2_ws/src/rosa_py_pkg/setup.py
+++ ros2_ws/src/rosa_py_pkg/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-           'py_node = rosa_py_pkg.screen_node:main'
+           'screen_node = rosa_py_pkg.screen_node:main'
         ],
     },
 )
